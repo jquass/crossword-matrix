d5598f7be1076575d5c97343ad48a6058a38cfeb
Jon Quass <quassj@gmail.com>
2021-03-26 15:15:35 -0400

    Start saving and updating puzzles

--- index.php
+++ index.php
@@ -1,37 +1,79 @@
 <?php
 
 require 'crossword_matrix/crossword_matrix_methods.php';
+require 'crossword_matrix/crossword_matrix_db.php';
 
-$name = array_key_exists('name', $_REQUEST)
-    ? $_REQUEST['name']
-    : 'New Puzzle';
-$size = array_key_exists('size', $_REQUEST)
-    ? $_REQUEST['size']
-    : 15;
+const DEFAULT_PUZZLE_NAME = 'New Puzzle';
+const PUZZLE_SIZE = 15;
 
-$puzzle = getPuzzleFromRequest($_REQUEST, $size);
+//
+// SETUP
+//
+
+$puzzle = getPuzzleFromRequest($_REQUEST, PUZZLE_SIZE);
 
 $template = findTemplate($puzzle);
 
-$twoDimensionalPuzzle = [];
-foreach ($puzzle as $puzzlePiece ) {
-    $twoDimensionalPuzzle += $puzzlePiece;
+$oneDimensionalPuzzle = [];
+foreach ($puzzle as $puzzlePiece) {
+    $oneDimensionalPuzzle += $puzzlePiece;
 }
 
+$name = array_key_exists('name', $_REQUEST)
+    ? $_REQUEST['name']
+    : DEFAULT_PUZZLE_NAME;
+
+$puzzleId = array_key_exists('id', $_REQUEST)
+    ? $_REQUEST['id']
+    : null;
+
+$savedPuzzle = $puzzleId
+    ? getSavedPuzzle($puzzleId)
+    : null;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if ($puzzleId) {
+        print '<h3>...updating...</h3>';
+
+        $savedPuzzle = updateSavedPuzzle($puzzleId, $name, $puzzle);
+        $success = $savedPuzzle ? true : false;
+
+    } else {
+        print '<h3>...saving...</h3>';
+
+        $puzzleId = savePuzzle($name, $puzzle);
+        $success = $puzzleId ? true : false;
+    }
+
+    print $success
+        ? '<h4>SUCCESS</h4>'
+        : '<h4>ERROR</h4><pre>' . pg_last_error() . '</pre>';
+
+} else if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    die('invalid request method : ' . $_SERVER['REQUEST_METHOD']);
+}
+
+//
+// HTML
+//
 ?>
 
 <form name="<?= $name ?>" method="post">
 
+    <input type="hidden" name="id" value="<?= $puzzleId ?>">
+
     <input type="text" name="name" style="position:absolute;top:35px;left:400px;" value="<?= $name ?>">
 
-    <input type="submit" style="position:absolute;top:75px;left:400px;" value="submit" name="submit"><br>
+    <input type="submit" style="position:absolute;top:75px;left:400px;" value="<?= $puzzleId ? 'Update' : 'Save' ?>"
+           name="submit"><br>
 
     <?php
     $n = 1;
-    while ($n <= $size * $size) {
+    while ($n <= PUZZLE_SIZE * PUZZLE_SIZE) {
         $name = 'c' . $n;
 
-        $value = array_key_exists($name, $twoDimensionalPuzzle) ? $twoDimensionalPuzzle[$name] : BLANK;
+        $value = array_key_exists($name, $oneDimensionalPuzzle) ? $oneDimensionalPuzzle[$name] : BLANK;
 
         if (array_key_exists($name, $template)) {
             $backgroundColor = 'yellow';
@@ -47,7 +89,7 @@ foreach ($puzzle as $puzzlePiece ) {
                     name=\"{$name}\" 
                     value=\"{$value}\">";
 
-        echo $n % $size == 0 ? '<br/>' : '';
+        echo $n % PUZZLE_SIZE == 0 ? '<br/>' : '';
 
         $n++;
     }
@@ -66,8 +108,13 @@ foreach ($puzzle as $puzzlePiece ) {
     <?= print_r($puzzle) ?>
 </pre>
 
-<h2>2D Puzzle</h2>
+<h2>1D Puzzle</h2>
 <pre>
-    <?= print_r($twoDimensionalPuzzle) ?>
+    <?= print_r($oneDimensionalPuzzle) ?>
+</pre>
+
+<h2>Saved Puzzle</h2>
+<pre>
+    <?= print_r($savedPuzzle) ?>
 </pre>
 
